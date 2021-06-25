5da15bce4c6699f35bb1dc579193c7deafeda442
brad@smithwebtek.com <brad@smithwebtek.com>
2021-06-25 13:22:50 -0400

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from './bjs.jpeg';
 import './App.css';
 
 function App() {
@@ -6,17 +6,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h2>
+          Hats found on the side of the road dot com
+        </h2>
       </header>
     </div>
   );
