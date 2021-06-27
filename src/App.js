58381cb43ceb4293a520932dbc0662a465df8a76
brad@smithwebtek.com <brad@smithwebtek.com>
2021-06-27 16:52:40 -0400

    Add highway

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import logo from './hat.svg';
+import hat from './hat.svg';
+import highway from './highway.svg'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h2>
+      <img src={highway} className="App-highway" alt="highway" />
+      <img src={hat} className="App-hat" alt="hat" />
+      <h1>
           Hats found on the side of the road (dot com)
-        </h2>
-      </header>
+      </h1>
     </div>
   );
 }
